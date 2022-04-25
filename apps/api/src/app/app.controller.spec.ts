610223fdafa5fd9510c9e96624c41eca6b0a9e71
zaunermax <zaunermax@gmail.com>
2022-04-25 11:56:31 +0000

    chore(format): formatted files

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -4,19 +4,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 describe('AppController', () => {
-  let app: TestingModule;
+	let app: TestingModule;
 
-  beforeAll(async () => {
-    app = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
-    }).compile();
-  });
+	beforeAll(async () => {
+		app = await Test.createTestingModule({
+			controllers: [AppController],
+			providers: [AppService],
+		}).compile();
+	});
 
-  describe('getData', () => {
-    it('should return "Welcome to api!"', () => {
-      const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
-    });
-  });
+	describe('getData', () => {
+		it('should return "Welcome to api!"', () => {
+			const appController = app.get<AppController>(AppController);
+			expect(appController.getData()).toEqual({ message: 'Welcome to api!' });
+		});
+	});
 });
