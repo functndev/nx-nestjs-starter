50d4ff6b3a183ebfcfdade32134c4d048fd9bbb7
zaunermax <zaunermax@gmail.com>
2022-05-12 12:11:46 +0000

    feat(tags): added linting tags

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,13 +5,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	const globalPrefix = 'api';
-	app.setGlobalPrefix(globalPrefix);
 	const port = process.env.PORT || 3333;
 	await app.listen(port);
-	Logger.log(
-		`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-	);
+	Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
