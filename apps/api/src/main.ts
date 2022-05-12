c032068168722d47417fe9be9ef68f623ddf3349
zaunermax <zaunermax@gmail.com>
2022-05-12 12:25:48 +0000

    feat(main): prepare main.ts for more functionality

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,29 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
+
+import { ConfigService, PrismaService } from '@nestjs-starter/api/modules/global';
 
 import { AppModule } from './app/app.module';
+import { performance } from 'perf_hooks';
+
+const now = performance.now();
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	const port = process.env.PORT || 3333;
-	await app.listen(port);
-	Logger.log(`🚀 Application is running on: http://localhost:${port}`);
+	const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+	const config = app.get(ConfigService);
+	const logger = app.get(Logger);
+	const prisma = app.get(PrismaService);
+
+	prisma.enableShutdownHooks(app);
+
+	app.useLogger(logger);
+
+	await app.listen(config.port);
+
+	logger.log(`App running in [${config.nodeEnv}] mode`);
+	logger.log(`Listening at: ${await app.getUrl()}`);
+	logger.log(`App took ${Math.round(performance.now() - now)}ms to start.`);
 }
 
 bootstrap();
