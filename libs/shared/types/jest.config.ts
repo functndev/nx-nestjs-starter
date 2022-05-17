89dc47c1bd5ef42a87a7e172bc836e849a8f8db9
zaunermax <zaunermax@gmail.com>
2022-05-17 13:50:05 +0200

    chore(nx): upgraded nx to the latest version

--- libs/shared/types/jest.config.ts
+++ libs/shared/types/jest.config.ts
@@ -1,6 +1,6 @@
-module.exports = {
+export default {
 	displayName: 'shared-types',
-	preset: '../../../jest.preset.ts',
+	preset: '../../../jest.preset.js',
 	globals: {
 		'ts-jest': {
 			tsconfig: '<rootDir>/tsconfig.spec.json',
