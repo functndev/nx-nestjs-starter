88dfc4303e8e34717095196423a3e64b9968675a
Maximilian Zauner <zaunermax@gmail.com>
2024-07-27 10:08:22 +0200

    feat(prisma): add custom prisma client location to improve builds
    - docker images are now smaller and built faster

--- libs/api/modules/global/src/services/prisma.service.ts
+++ libs/api/modules/global/src/services/prisma.service.ts
@@ -1,17 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
 
-import type { INestApplication, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@nestjs-starter/prisma';
+
+import type { OnModuleInit } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
 	public async onModuleInit() {
 		await this.$connect();
-	}
-
-	public enableShutdownHooks(app: INestApplication) {
-		this.$on('beforeExit', async () => {
-			await app.close();
-		});
 	}
 }
